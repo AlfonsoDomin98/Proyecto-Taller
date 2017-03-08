51adf7806e12db65686f4cbca5b5f3c94eb3fa3a
Alfonso <Alfonso@192.168.1.196>
2017-03-08 14:01:17 +0100

    Prueba antes Examen

---
+++ src/Clases/Coche.java
@@ -0,0 +1,18 @@
+package Clases;
+
+public class Coche extends Vehiculo {
+	private String Ventanillas;
+	
+	
+	public Coche(){
+		
+	}
+	
+	public void subirVentanillas(){
+		this.Ventanillas= "Subidas";
+	}
+	
+	public void bajarVentanillas(){
+		
+	}
+}
